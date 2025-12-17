ab6083a16428b6b75e522781fd2340595634d778
keremsenn <keremsen1071@gmail.com>
2025-12-17 17:14:48 +0300

    AuthApiService ile giriş ve kayıt işlemleri eklendi

---
+++ client/app/src/main/java/com/keremsen/e_commerce/api/AuthApiService.kt
@@ -0,0 +1,17 @@
+package com.keremsen.e_commerce.api
+
+import com.keremsen.e_commerce.models.requestModel.LoginRequest
+import com.keremsen.e_commerce.models.requestModel.RegisterRequest
+import com.keremsen.e_commerce.models.responseModel.AuthResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApiService {
+
+    @POST("api/auth/register")
+    suspend fun register(@Body request: RegisterRequest): Response<AuthResponse>
+
+    @POST("api/auth/login")
+    suspend fun login(@Body request: LoginRequest): Response<AuthResponse>
+}
