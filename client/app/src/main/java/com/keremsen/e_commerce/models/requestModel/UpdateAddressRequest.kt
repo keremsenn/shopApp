06e3337b588e4f7d809a07110bdd95d15470d766
keremsenn <keremsen1071@gmail.com>
2025-12-19 22:49:41 +0300

    gereksiz model silindi

--- client/app/src/main/java/com/keremsen/e_commerce/models/requestModel/UpdateAddressRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.keremsen.e_commerce.models.requestModel
-
-data class UpdateAddressRequest(
-    val title: String? = null,
-    val city: String? = null,
-    val district: String? = null,
-    val detail: String? = null
-)
