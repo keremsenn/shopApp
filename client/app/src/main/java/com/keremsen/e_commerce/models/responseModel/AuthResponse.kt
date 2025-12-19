af78ff3a3abade3fb0945a8029a01919d8efbc72
keremsenn <keremsen1071@gmail.com>
2025-12-19 22:51:03 +0300

    back endden gelen refresh tokenı alabılmke ıcın authresponse guncellendı

--- client/app/src/main/java/com/keremsen/e_commerce/models/responseModel/AuthResponse.kt
+++ client/app/src/main/java/com/keremsen/e_commerce/models/responseModel/AuthResponse.kt
@@ -5,6 +5,7 @@ import com.keremsen.e_commerce.models.entityModel.User
 data class AuthResponse(
     val user: User?,
     val access_token: String?,
+    val refresh_token:String?,
     val message: String? = null,
     val error: String? = null
 )
