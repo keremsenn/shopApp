3d2fd70993f282843adb153af8239362e42a9901
keremsenn <keremsen1071@gmail.com>
2025-12-20 21:34:43 +0300

    satıcı kullanıcı ayarlarımızı yanlız kullanıcı tabanlı ve müşteriye yonelık olan bır uygulamaya cevırdık

--- client/app/src/main/java/com/keremsen/e_commerce/models/requestModel/SellerRequestApply.kt
+++
@@ -1,6 +0,0 @@
-package com.keremsen.e_commerce.models.requestModel
-
-data class SellerRequestApply(
-    val company_name: String,
-    val tax_number: String
-)
--- client/app/src/main/java/com/keremsen/e_commerce/models/responseModel/SellerRequestResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.keremsen.e_commerce.models.responseModel
-
-import com.keremsen.e_commerce.models.requestModel.SellerRequest
-
-data class SellerRequestResponse(
-    val message: String?,
-    val error: String?,
-    val request: SellerRequest?
-)
